cca26698fdaad8bcb07ae0190d1e6434fcfbeb92
KingCoded <61747942+KingCoded@users.noreply.github.com>
2020-03-03 23:40:17 -0500

    Add files via upload
    First Time for EVERYTHING!@

---
+++ P2T1_SalesPrediction_ElventreSoutherland.py
@@ -0,0 +1,15 @@
+#This program will calculate annual profit at 23%
+#2/20/20
+#CTI-110 P2T1 - Sales Prediction
+#Elventre' Southerland
+#
+
+#Get the projected total sales
+total_sales = float(input('Enter the projected sales: '))
+
+#Calculate the profit as 23 percent of total sales
+profit = total_sales * 0.23
+
+#Display the profit
+print('The profit is $', format(profit, ',.2f'))
+
